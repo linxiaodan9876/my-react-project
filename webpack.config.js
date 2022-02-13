0623f6a62b1e458d243fd7903fed6c49b876c603
linxiaodan9876 <1172321315@qq.com>
2022-02-13 17:25:03 +0800

    feat: 添加两个可移动的浮球。

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require("path");
 // 插件都是一个类，所以我们命名的时候尽量用大写开头
 let HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: "./main.js", // webpack打包的入口文件
+  entry: path.join(__dirname, "./main.js"), // webpack打包的入口文件
   output: {
+    path: path.join(__dirname, "./dist"), // 打包后的目录，必须是绝对路径。写法一
+    // path: path.resolve("dist"), // 打包后的目录，必须是绝对路径。写法二
     filename: "bundle.js", // 输出之后的文件名
-    path: path.resolve("dist"), // 打包后的目录，必须是绝对路径
   },
   mode: "development",
   module: {
@@ -25,6 +26,7 @@ module.exports = {
         use: [
           // css-loader和style-loader，对css后缀的文件进行处理
           // style-loader必须在css-loader前面
+          // 调用顺序是从下至上（or从右向左）的
           { loader: "style-loader" },
           { loader: "css-loader" },
           { loader: "sass-loader" },
@@ -36,7 +38,7 @@ module.exports = {
     // 通过new一下这个类来使用插件
     new HtmlWebpackPlugin({
       // 在src目录下创建一个index.html页面当做模板来用
-      template: "./index.html",
+      template: path.join(__dirname, "./index.html"),
       hash: true, // 会在打包好的bundle.js后面加上hash串
     }),
   ],
@@ -45,5 +47,6 @@ module.exports = {
     static: {
       directory: path.join(__dirname),
     },
+    port: 8181,
   },
 };
